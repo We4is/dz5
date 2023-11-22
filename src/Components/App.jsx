0b277f36ab05844dac1e925319f9598507bdfd04
We4is <silkonikita009@gmail.com>
2023-11-22 17:50:01 +0300

    Add existing file

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,37 +1,76 @@
 import { useState } from 'react'
 import FoodBox from './FoodBox/FoodBox.jsx'
-import '../Style/app.css'
+import '../Style/app.scss'
 import burderImg from '../assets/burger.png'
 import potatoImg from '../assets/potato.png'
 import sausageImg from '../assets/sausage.png'
 
 function App() {
+  const [count, setCount] = useState(3)
+  const [sum, setSum] = useState(1034)
   const [FoodInfo, setFoodInfo] = useState([
-    {
+    { 
       name: 'Супер сырный',
       weight: '512г',
-      price: '550Р',
-      img: burderImg
+      price: 550,
+      img: burderImg,
+      id: 1
     },
     {
       name: 'Картошка фри',
       weight: '180г',
-      price: '245Р',
-      img: potatoImg
+      price: 245,
+      img: potatoImg,
+      id: 2
     },
     {
       name: 'Жгучий хот-дог',
       weight: '245г',
-      price: '239Р',
-      img: sausageImg
+      price: 239,
+      img: sausageImg,
+      id: 3
     }
   ])
 
+
+
+  function editCount(amper,price){
+    setCount(count + amper)
+    editSum(price)
+  }
+
+  function editSum(price){
+    setSum(sum + price)
+  }
+
+  function deleteFood(id, price, number){
+        const newArr = [...FoodInfo]
+        const newFoodArr = newArr.filter((item) => item.id != id)
+        setFoodInfo(newFoodArr);
+        editCount(number,price)
+        editSum(-(price * number))
+  }
+
+
   return (
     <>
-      {FoodInfo.map((item) =>{
-        return <FoodBox {...item}/>
-      })}
+      <div className="foodBlock">
+        <div className="food_text">
+          <h1>Корзина</h1>
+          <p>{count}</p>
+        </div>
+        <div className="food_blocks">
+          {FoodInfo.map((item) => {
+            return <FoodBox {...item} key={item.id} editCount={editCount} deleteFood={deleteFood}/>
+          })}
+        </div>
+        <div className='food_footer'>
+          <p>Итого</p>
+          <span>{sum}</span>
+        </div>
+
+      </div>
+
     </>
   )
 }
--- src/Components/FoodBox/FoodBox.jsx
+++ src/Components/FoodBox/FoodBox.jsx
@@ -1,31 +1,39 @@
 import { useState } from 'react'
 import './FoodBox.scss'
 
-function FoodBox({ name, weight, price, img }) {
+function FoodBox({ name, weight, price, img, editCount, id, deleteFood, changeSum }) {
     const [number, setNumber] = useState(1)
-    function changeNumDown() {
-        if (number !== 1) {
-            setNumber(number - 1)
+    function changeNum(amper, price) {
+        if (number == 1 && String(amper) === '-1') {
+            deleteFood(id, number)
         }
+        console.log(price);
+        setNumber(number + amper)
+        editCount(amper,price)
     }
-    function changeNumUp() {
-        setNumber(number + 1)
-    }
+
     return (
         <>
             <div className="food_box">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" className='close_btn' onClick={() =>{
+                    deleteFood(id, -price, -number)
+                }}><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
                 <div className='food_info'>
                     <img src={img} alt="" />
                     <div className="food_info-text">
                         <h5>{name}</h5>
                         <span>{weight}</span>
-                        <p>{price}</p>
+                        <p>{price}Р</p>
                     </div>
                 </div>
                 <div className="calc">
-                    <button onClick={changeNumDown}>-</button>
+                    <button onClick={() => {
+                        changeNum(-1,-price)
+                    }}>-</button>
                     <p>{number}</p>
-                    <button onClick={changeNumUp}>+</button>
+                    <button onClick={() => {
+                        changeNum(+1,+price)
+                    }}>+</button>
                 </div>
             </div>
         </>
